c3479eb4fc726bc9f7321e1ea61f8bc465aa8895
gaobingyi <bingyi.gao@foxmail.com>
2017-10-16 23:29:39 +0800

    Update Main7.java

--- Main7.java
+++ Main7.java
@@ -6,19 +6,19 @@ public class Main7 {
 	public static void main(String[] args) {
 		// Input matrix
 		Scanner in = new Scanner(System.in);
-        String[] rc = in.nextLine().split(" ");
-        int row = Integer.valueOf(rc[0]);
-        int col = Integer.valueOf(rc[1]);
-        int[][] matrix = new int[row][col];
-        for (int i = 0; i < row; i++) {
-        	String[] nums = in.nextLine().split(" ");
-        	for (int j = 0; j < col; j++) {
-        		matrix[i][j] = Integer.valueOf(nums[j]);
-        	}
-        }
-        in.close();
-        // Print matrix
-        printMatrix(matrix, row, col);
+		String[] rc = in.nextLine().split(" ");
+		int row = Integer.valueOf(rc[0]);
+		int col = Integer.valueOf(rc[1]);
+		int[][] matrix = new int[row][col];
+		for (int i = 0; i < row; i++) {
+			String[] nums = in.nextLine().split(" ");
+			for (int j = 0; j < col; j++) {
+				matrix[i][j] = Integer.valueOf(nums[j]);
+			}
+		}
+		in.close();
+		// Print matrix
+		printMatrix(matrix, row, col);
 	}
 	
 	public static void printMatrix(int[][] matrix, int row, int col) {
